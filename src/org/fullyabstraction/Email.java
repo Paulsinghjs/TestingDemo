333011b07eb78706e4294fb08080b5ac0809e97d
Paulsinghjs <pauljs007@gmail.com>
2021-07-07 18:05:27 +0530

    email commit

---
+++ src/org/fullyabstraction/Email.java
@@ -0,0 +1,12 @@
+package org.fullyabstraction;
+
+public class Email {
+ 
+	public static void userName() {
+	System.out.println("Username");
+
+}
+	public static void main(String[] args) {
+		userName();
+	}
+}
