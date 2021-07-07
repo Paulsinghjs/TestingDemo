60fc2add79bb59ff0e3e5cb7f154c1ec4157067b
Paulsinghjs <pauljs007@gmail.com>
2021-07-07 18:29:59 +0530

    email commit

---
+++ src/org/fullyabstraction/FbLogin.java
@@ -0,0 +1,12 @@
+package org.fullyabstraction;
+
+public class FbLogin {
+
+	public static void userName() {
+		System.out.println("Username");
+
+	}
+		public static void main(String[] args) {
+			userName();
+		}
+}
