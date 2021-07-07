bbd63c75f57a7892bb1eb7c6d13260563b7c0b96
Paulsinghjs <pauljs007@gmail.com>
2021-07-07 18:27:07 +0530

    pwd commit

---
+++ src/org/fullyabstraction/FbLogin.java
@@ -0,0 +1,12 @@
+package org.fullyabstraction;
+
+public class FbLogin {
+	
+	public static void passWord() {
+		System.out.println("Password");
+
+	}
+		public static void main(String[] args) {
+			passWord();
+		}
+}
