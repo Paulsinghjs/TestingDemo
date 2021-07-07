24a7ab6e0f8ce9078cab8cafd78a3d8028050ec2
Paulsinghjs <pauljs007@gmail.com>
2021-07-07 18:09:42 +0530

    pwd commit

---
+++ src/org/fullyabstraction/Pwd.java
@@ -0,0 +1,12 @@
+package org.fullyabstraction;
+
+public class Pwd {
+
+	public static void passWord() {
+		System.out.println("Password");
+
+	}
+	public static void main(String[] args) {
+		passWord();
+	}
+}
